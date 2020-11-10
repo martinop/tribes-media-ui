c8ece18f40190d9e5853d72ca8e24a3b7e200f1f
Martin Ocando <martinocandop@gmail.com>
2020-11-10 14:32:48 -0300

    Export compomnent fixed

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,13 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "button", {
+Object.defineProperty(exports, "CastMemberDetails", {
   enumerable: true,
   get: function get() {
-    return _button.default;
+    return _CastMemberDetails.default;
   }
 });
 
-var _button = _interopRequireDefault(require("./button"));
+var _CastMemberDetails = _interopRequireDefault(require("./CastMemberDetails"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,1 @@
-export { default as button } from './button';
+export { default as CastMemberDetails } from './CastMemberDetails';
