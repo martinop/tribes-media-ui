05d728fca0733e439a7b6f2ee14fb313900e741d
Martin Ocando <martinocandop@gmail.com>
2020-11-14 18:49:07 -0300

    Export languages modal

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,15 @@ Object.defineProperty(exports, "CastMemberDetails", {
     return _CastMemberDetails.default;
   }
 });
+Object.defineProperty(exports, "LanguagesModal", {
+  enumerable: true,
+  get: function get() {
+    return _LanguagesModal.default;
+  }
+});
 
 var _CastMemberDetails = _interopRequireDefault(require("./CastMemberDetails"));
+
+var _LanguagesModal = _interopRequireDefault(require("./LanguagesModal"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export { default as CastMemberDetails } from './CastMemberDetails';
+export { default as LanguagesModal } from './LanguagesModal';
